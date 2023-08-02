358980ab94fcd1f3a56f1620203d035b291a9708
matthew-tolentino-333 <matttolent@gmail.com>
2023-08-01 22:37:28 -0700

    Add Menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './styles.scss';
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
     <div className="App">
+      <Menu/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -16,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Idiot
         </a>
       </header>
     </div>
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Menu.scss';
+
+function Menu() {
+  return (
+    <div className="menu">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Portfolio</li>
+        <li>Resume</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
