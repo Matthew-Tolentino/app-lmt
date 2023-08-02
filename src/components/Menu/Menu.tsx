4f77d78fc7efded6c4be967afbf5e022e60c5ea6
matthew-tolentino-333 <matttolent@gmail.com>
2023-08-02 08:18:13 -0700

    Sliding panel mock 1

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Menu.scss';
 
 function Menu() {
+  const [open, setOpen] = useState(false);
+
+  const menuItems = ['Home', 'About', 'Skills', 'Portfolio', 'Resume/CV']
+
   return (
     <div className="menu">
-      <span className="material-icons menu-icon">
-        menu
-      </span>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Portfolio</li>
-        <li>Resume</li>
-      </ul>
+      <button
+        onClick={() => setOpen(!open)}
+        >
+        <span className="material-icons menu-icon">
+          menu
+        </span>
+      </button>
+      <div className={`menu-list ${open ? 'open' : ''}`}>
+        <ul>
+          {menuItems.map(item => {
+            return (
+              <li key={item} onClick={() => setOpen(false)}>
+                {item}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
