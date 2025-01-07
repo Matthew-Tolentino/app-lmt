e593ff7fad8e590cf5bc8b7391dac48fbfddead0
matthew-tolentino-333 <lemtolen@ucsc.edu>
2025-01-06 23:03:12 -0800

    update resume link

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -13,7 +13,7 @@ function Contact() {
           <li><a href="https://github.com/Matthew-Tolentino"><FiGithub /></a></li>
           <li><a href="https://www.linkedin.com/in/leonard-tolentino-b34398188/"><CiLinkedin /></a></li>
           <li><a href="mailto:lemtolen@gmail.com"><CiMail /></a></li>
-          <li><a href="https://drive.google.com/file/d/1KoOYgcAO3QZ88dHLof5XE30IUTmnw6c2/view?usp=sharing"><IoDocumentTextOutline /></a></li>
+          <li><a href="https://drive.google.com/file/d/1QTK4_CyqBeRpGCKgRl1JGaokOIQfBVTQ/view?usp=sharing"><IoDocumentTextOutline /></a></li>
         </ul>
       </div>
     </>
